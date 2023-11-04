9b81568d2a02a124c372bc0c22687dbdca48bfac
jbescos <jorgebescos@gmail.com>
2023-11-04 12:02:18 +0100

    MINST example with neuroph
    Signed-off-by: jbescos <jorgebescos@gmail.com>

--- src/test/java/es/tododev/ml/mine/MnistNumbersTest.java
+++ src/test/java/es/tododev/ml/mine/MnistNumbersTest.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -15,9 +16,14 @@ import java.util.zip.ZipInputStream;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.neuroph.core.Layer;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.Neuron;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.nnet.learning.BackPropagation;
+import org.neuroph.util.ConnectionFactory;
 
-import es.tododev.ml.mine.Net;
-import es.tododev.ml.mine.Trainer;
 import es.tododev.ml.mine.Trainer.TestData;
 
 public class MnistNumbersTest {
@@ -26,7 +32,7 @@ public class MnistNumbersTest {
 
     @Test
     @Ignore
-    public void predictNumbers() throws IOException, ClassNotFoundException {
+    public void predictNumbersMine() throws IOException, ClassNotFoundException {
         List<TestData> train = fromZip("/mnist_train.zip");
         int inputs = train.get(0).getIn().length;
         int outputs = train.get(0).getOut().length;
@@ -55,6 +61,75 @@ public class MnistNumbersTest {
         assertEquals(Float.toString(best.cost(train)), Float.toString(loaded.cost(train)));
     }
 
+    @Test
+    public void predictNumbersNeuroph() throws IOException, ClassNotFoundException {
+        List<TestData> train = fromZip("/mnist_train.zip");
+        int inputs = train.get(0).getIn().length;
+        int outputs = train.get(0).getOut().length;
+
+        NeuralNetwork ann = new NeuralNetwork();
+        addLayer(ann, inputs);
+        addLayer(ann, 10);
+        addLayer(ann, 10);
+        addLayer(ann, outputs);
+        ann.setInputNeurons(ann.getLayers()[0].getNeurons());
+        ann.setOutputNeurons(ann.getLayers()[ann.getLayersCount() - 1].getNeurons());
+        train(ann, train);
+
+        int successN = 0;
+        int total = 0;
+
+        train = fromZip("/mnist_test.zip");
+        for (TestData data : train) {
+            float[] in = data.getIn();
+            float[] out = data.getOut();
+            double[] inD = IntStream.range(0, in.length).mapToDouble(i -> in[i]).toArray();
+            double[] outD = IntStream.range(0, out.length).mapToDouble(i -> out[i]).toArray();
+            ann.setInput(inD);
+            ann.calculate();
+            double[] result = ann.getOutput();
+            boolean success = Arrays.equals(outD, result);
+            total++;
+            if (success) {
+                successN++;
+            }
+        }
+        System.out.println("Total: " + total + ", Success: " + successN + ", performance: " + (successN / total));
+    }
+
+    private void train(NeuralNetwork ann, List<TestData> train) {
+        int inputs = train.get(0).getIn().length;
+        int outputs = train.get(0).getOut().length;
+        DataSet ds = new DataSet(inputs, outputs);
+        for (TestData data : train) {
+            float[] in = data.getIn();
+            float[] out = data.getOut();
+            double[] inD = IntStream.range(0, in.length).mapToDouble(i -> in[i]).toArray();
+            double[] outD = IntStream.range(0, out.length).mapToDouble(i -> out[i]).toArray();
+            assertEquals(inputs, inD.length);
+            assertEquals(outputs, outD.length);
+            DataSetRow row = new DataSetRow(inD, outD);
+            ds.addRow(row);
+        }
+        BackPropagation backPropagation = new BackPropagation();
+        backPropagation.setMaxIterations(1000);
+        System.out.println("Learning ...");
+        ann.learn(ds, backPropagation);
+    }
+    
+    private void addLayer(NeuralNetwork ann, int size) {
+        Layer layer = new Layer();
+        for (int i = 0; i < size; i++) {
+            layer.addNeuron(new Neuron());
+        }
+        ann.addLayer(layer);
+        assertEquals(size, ann.getLayerAt(ann.getLayersCount() - 1).getNeuronsCount());
+        int layerCount = ann.getLayersCount();
+        if (layerCount > 1) {
+            ConnectionFactory.fullConnect(ann.getLayerAt(layerCount - 2), ann.getLayerAt(layerCount - 1));
+        }
+    }
+    
     private List<TestData> fromZip(String resource) throws IOException {
         List<TestData> test = new ArrayList<>();
         try (InputStream in = MnistNumbersTest.class.getResourceAsStream(resource);
